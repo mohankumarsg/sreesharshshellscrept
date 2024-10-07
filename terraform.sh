1b58412821e459fd862c193e520d44da2fa5af98
mohankumarsg <mohankumarsg.it@gmail.com>
2024-10-07 15:31:18 +0530

    test2

--- terraform.sh
+++ terraform.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-T_version='1.8.5'
+T_version='1.9.0'
 wget https://releases.hashicorp.com/terraform/${T_version}/terraform_${T_version}_linux_386.zip -o terraform.zip
-unzip terraform.zip && rm -rf terraform.zip
-echo "remove the zip file .."
-rm -rf  /usr/local/bin/terraform
-chmoh 777 terraform
-mv terraform /usr/local/bin/
-terraform --version
-echo " terraform version.."
+#unzip terraform.zip && rm -rf terraform.zip
+#echo "remove the zip file .."
+#rm -rf  /usr/local/bin/terraform
+#chmoh 700 terraform
+#mv terraform /usr/local/bin/
+#terraform --version
+#echo " terraform version.."
