11d193d685ad525c1de5074bae33f9b457ea46fa
mohankumarsg <mohankumarsg.it@gmail.com>
2024-10-19 19:36:22 +0530

    test

---
+++ newparameters1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "$# number of argumets"
+for app in "$*"
+do 
+command -v $app >> /dev/null 
+if [ $? == 0 ];
+then 
+echo "$app installed"
+ else
+ echo "$app not installed"
+ fi
+ done 
